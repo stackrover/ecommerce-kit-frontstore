4e524a6ed8046157ca16088b58b612c49ae3fc8e
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2025-09-24 23:20:44 +0600

    chome: testing pre-commit code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-import { Icon } from "../shared/components/Icon";
+import { Icon } from '@/shared/components/Icon';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className="">
       <Icon name="LeafIcon" className="h-6 w-6 text-blue-500" />
-
-      <Icon
-        name="BookOpenIcon"
-        library="heroicons"
-        className="h-6 w-6 text-green-500"
-      />
-
-      <Icon
-        name="IconBrandArc"
-        library="tabler"
-        className="h-6 w-6 text-red-500"
-      />
-
+      <Icon name="BookOpenIcon" library="heroicons" className="h-6 w-6 text-green-500" />
+      <Icon name="IconBrandArc" library="tabler" className="h-6 w-6 text-red-500" />
       <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
     </div>
   );
